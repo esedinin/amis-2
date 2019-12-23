167f7270570304e182a47a879d0c5d9f4c523235
Yehor Sedinin <yehor.sedinin@competera.net>
2019-12-23 22:51:31 +0200

    Added db setup

--- app.py
+++ app.py
@@ -15,6 +15,10 @@ import plotly.graph_objs as go
 import os
 
 db = PostgresDb()
+
+Base.metadata.create_all(db.sqlalchemy_engine)
+
+session = db.sqlalchemy_session
 app = Flask(__name__)
 app.secret_key = os.getenv("SECRET_KEY", "jkm-vsnej9l-vm9sqm3:lmve")
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL",
