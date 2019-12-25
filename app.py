e9d462a35e0436071382b95e42b4c4150a2b6cc2
Yehor Sedinin <yehor.sedinin@competera.net>
2019-12-25 12:48:42 +0200

    Improve editing

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 from source.dao.orm.entities import *
-# from source.dao.orm.populate import *
+from source.dao.orm.populate import *
 from source.dao.db import PostgresDb
 from datetime import date
 from source.dao.data import *
